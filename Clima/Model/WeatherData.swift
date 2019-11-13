3b6132366d614ba8ba5e0fc6b0f2af05c6417f32
rasyidrw <rasyidrespati.30@gmail.com>
2019-11-13 17:18:26 +0700

    implement JSON Decoder

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Rasyid Respati Wiriaatmaja on 13/11/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -18,31 +18,36 @@ struct WeatherManager {
     
     func performRequest(urlString: String) {
         //1. Create a URL
-        
         if let url = URL(string: urlString) {
-            //2. Create a URLSession
             
+            //2. Create a URLSession
             let session = URLSession(configuration: .default)
             
             //3. Give the session a task
-            
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             
             //4. Start the task
-            
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
+    
 }
