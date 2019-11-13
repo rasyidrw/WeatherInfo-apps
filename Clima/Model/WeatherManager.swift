395e0ce55a45df16de3cb14312464bf36afe94f5
rasyidrw <rasyidrespati.30@gmail.com>
2019-11-13 18:26:39 +0700

    implement computed properties

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -44,10 +44,18 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let name = decodedData.name
+            let temp = decodedData.main.temp
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
     }
+    
+    
     
 }
